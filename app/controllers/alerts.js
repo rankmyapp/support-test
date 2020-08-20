ef7c713cc754e4e13729501bb737b66d824b0d28
Erick Malta <erickmalta100@gmail.com>
2020-08-20 19:42:50 -0300

    feat: :ok_hand: add joi validation to update method

--- app/controllers/alerts.js
+++ app/controllers/alerts.js
@@ -1,7 +1,11 @@
-const Alert = require('../models/alert');
-const { saveSchema } = require('../validators/alerts');
-const { HTTP } = require('../helpers/constants');
-const { joiErrorHandling, mongoErrorHandling, commonErrorHandling } = require('../helpers/error-handler');
+const Alert = require("../models/alert");
+const { saveSchema } = require("../validators/alerts");
+const { HTTP } = require("../helpers/constants");
+const {
+  joiErrorHandling,
+  mongoErrorHandling,
+  commonErrorHandling,
+} = require("../helpers/error-handler");
 
 const find = async (req, res) => {
   const alerts = await Alert.find({});
@@ -11,7 +15,7 @@ const find = async (req, res) => {
 const findById = async (req, res) => {
   const { id } = req.params;
 
-  const alert = await Alert.findById(id)
+  const alert = await Alert.findById(id);
   return res.json({ alert });
 };
 
@@ -19,43 +23,46 @@ const save = async (req, res) => {
   const { email, term } = req.body;
   const { error } = saveSchema.validate(req.body);
   if (error) {
-    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(
-      joiErrorHandling(error)
-    );
+    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(joiErrorHandling(error));
   }
 
   const alert = await Alert.findOne({ email, term });
   if (alert) {
-    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(
-      commonErrorHandling('term', 'Term already registered for this user')
-    );
+    return res
+      .status(HTTP.UNPROCESSABLE_ENTITY)
+      .json(
+        commonErrorHandling("term", "Term already registered for this user")
+      );
   }
 
   try {
-    const alert = await Alert.create({ ...req.body })
+    const alert = await Alert.create({ ...req.body });
     return res.status(HTTP.CREATED).json({ alert });
-  } catch(err){
-    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(
-      mongoErrorHandling(err)
-    );
+  } catch (err) {
+    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(mongoErrorHandling(err));
   }
 };
 
 const update = async (req, res) => {
   const { id } = req.params;
+  // Alteração
+  const { error } = saveSchema.validate(req.body);
+  if (error) {
+    return res.status(HTTP.UNPROCESSABLE_ENTITY).json(joiErrorHandling(error));
+  }
   const { email, frequency, term } = req.body;
 
   const alert = await Alert.findById(id);
   if (!alert) {
-    return res.status(HTTP.NOT_FOUND).json(
-      commonErrorHandling('_id', 'Alert not found')
-    );
+    return res
+      .status(HTTP.NOT_FOUND)
+      .json(commonErrorHandling("_id", "Alert not found"));
   }
 
   alert.overwrite({
     email: email || alert.email,
     frequency: frequency || alert.frequency,
-    term: term || alert.term
+    term: term || alert.term,
   });
 
   try {
@@ -70,21 +77,21 @@ const remove = async (req, res) => {
   const { id } = req.params;
 
   try {
-    await Alert.deleteOne({ _id: id })
+    await Alert.deleteOne({ _id: id });
 
     return res.json({
       _id: id,
-      message: 'Notification successfully deleted'
+      message: "Notification successfully deleted",
     });
   } catch (err) {
     return res.status(HTTP.ERROR).json({ message: err });
   }
-}
+};
 
 module.exports = {
   find,
   findById,
   save,
   update,
-  remove
+  remove,
 };
